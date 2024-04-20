3fcbc7fffd52e88afde23d2b8de0be47702f8969
connie <50786998+connie0714@users.noreply.github.com>
2024-04-20 10:22:14 +0900

    Start
    시작

---
+++ Lucid_Taro/src/main/java/com/lucid/taro/LucidTaroApplication.java
@@ -0,0 +1,13 @@
+package com.lucid.taro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucidTaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucidTaroApplication.class, args);
+	}
+
+}
---
+++ Lucid_Taro/src/test/java/com/lucid/taro/LucidTaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucid.taro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucidTaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
